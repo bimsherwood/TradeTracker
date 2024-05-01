c05da332e6129355c2217321e2f8e7565c38fc78
Bim Sherwood <bimmosherwood@gmail.com>
2024-05-01 20:33:19 +1000

    Testing maui shell tabs

---
+++ TradeTracker/Page/Settings.xaml.cs
@@ -0,0 +1,18 @@
+﻿using TradeTracker.Services;
+using TradeTracker.ViewModel;
+
+namespace TradeTracker.Page;
+
+public partial class Settings : ContentPage
+{
+	
+    private BalanceSheetViewModel ViewModel;
+
+	public Settings()
+	{
+		var database = DependencyService.Get<DataService>();
+		InitializeComponent();
+	}
+
+}
+
